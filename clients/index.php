93c6e6cd471f81fc7993762c95aba6f63c8a09b6
Yaroslav <kseltas28@gmail.com>
2024-09-12 00:06:09 +0300

    Added new elements and styles

--- clients/index.php
+++ clients/index.php
@@ -1,8 +1,23 @@
 <html>
 	<head>
-		<link rel = "stylesheet" href = "styles.css">
+		<link rel = "stylesheet" href = "css/styles.css">
 	</head>
 	<body>
-		<div class = "banners-div"></div>
+		<div class = "banners-div">			
+			
+			<div class = "banners-div-item item-element hover-item">
+				<p>1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum neque urna, cursus ac metus at, blandit viverra tortor. Quisque rhoncus in nunc eu aliquam. Sed diam ex, euismod et eros nec, lobortis accumsan neque. Cras commodo dui non tortor condimentum placerat. Ut sem dolor, consectetur vitae vulputate quis, posuere in tortor. Maecenas blandit posuere dui, a laoreet tortor placerat eu. Vestibulum et enim in risus scelerisque mollis. </p>
+
+			</div>
+			<div class = "banners-div-item flex-item ">
+				<div class ="flex-item-element hover-item">
+					<p>2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum neque urna, cursus ac metus at, blandit viverra tortor. Quisque rhoncus in nunc eu aliquam. Sed diam ex, euismod et eros nec, lobortis accumsan neque. Cras commodo dui non tortor condimentum placerat. Ut sem dolor, consectetur vitae vulputate quis, posuere in tortor. Maecenas blandit posuere dui, a laoreet tortor placerat eu. Vestibulum et enim in risus scelerisque mollis. </p>
+				</div>
+				<div class ="flex-item-element hover-item">
+					<p>3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum neque urna, cursus ac metus at, blandit viverra tortor. Quisque rhoncus in nunc eu aliquam. Sed diam ex, euismod et eros nec, lobortis accumsan neque. Cras commodo dui non tortor condimentum placerat. Ut sem dolor, consectetur vitae vulputate quis, posuere in tortor. Maecenas blandit posuere dui, a laoreet tortor placerat eu. Vestibulum et enim in risus scelerisque mollis. </p>
+				</div>
+				
+			</div>
+		</div>
 	</body>
 </html>
